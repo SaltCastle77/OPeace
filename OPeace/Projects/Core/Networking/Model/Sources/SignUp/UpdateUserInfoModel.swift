44e5b3820b1e5b3eaf8dfb9377465b18665e5b45
Roy <shuwj81@daum.net>
2024-07-30 22:59:11 +0900

    ✨[feat]:  유정 정보 업데이트 관련 #7

---
+++ OPeace/Projects/Core/Networking/Model/Sources/SignUp/UpdateUserInfoModel.swift
@@ -0,0 +1,55 @@
+//
+//  UpdateNickNameModel.swift
+//  Model
+//
+//  Created by 서원지 on 7/30/24.
+//
+
+import Foundation
+
+// MARK: - Welcome
+public struct UpdateProfileModel: Codable, Equatable {
+    public let data: UpdateProfileResponse?
+    
+    public init(data: UpdateProfileResponse?) {
+        self.data = data
+    }
+    
+}
+
+// MARK: - DataClass
+public struct UpdateProfileResponse: Codable, Equatable {
+    let nickname, year, generation, job: Generation?
+    
+    public enum CodingKeys: String, CodingKey {
+        case nickname
+        case year
+        case generation
+        case job
+    }
+    
+    public init(
+        nickname: Generation?,
+        year: Generation?,
+        generation: Generation?,
+        job: Generation?
+    ) {
+        self.nickname = nickname
+        self.year = year
+        self.generation = generation
+        self.job = job
+    }
+}
+
+// MARK: - Generation
+public struct Generation: Codable , Equatable{
+    let message: String?
+    
+    public init(message: String?) {
+        self.message = message
+    }
+    
+    public enum CodingKeys: String, CodingKey {
+        case message
+    }
+}
