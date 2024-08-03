3a17e3605e3e5f5d91880835a485c3fcd6db0cd2
Roy <shuwj81@daum.net>
2024-08-04 03:04:58 +0900

    🪛[chore]:  파일 위치 변경 #9

---
+++ OPeace/Projects/Presentation/Presentation/Sources/Auth/SignUpPaging/Reducer/SignUpPaging.swift
@@ -0,0 +1,209 @@
+//
+//  SignUpPaging.swift
+//  Presentation
+//
+//  Created by 서원지 on 7/24/24.
+//
+
+import Foundation
+import ComposableArchitecture
+
+import Utill
+import Model
+import DesignSystem
+import Utills
+import UseCase
+
+@Reducer
+public struct SignUpPaging {
+    public init() {}
+    
+    @ObservableState
+    public struct State: Equatable {
+        public init() {}
+        
+         var signUpName = SignUpName.State()
+         var signUpAge = SignUpAge.State()
+        var signUpAges = SignUpAge.State()
+        var signUpJob = SignUpJob.State()
+        var activeMenu: SignUpTab = .signUpName
+        
+        var updateUserinfoModel: UpdateUserInfoModel?
+        @Presents var destination: Destination.State?
+        
+        
+    }
+    
+    public enum Action: ViewAction, FeatureAction, BindableAction {
+        case binding(BindingAction<State>)
+        case destination(PresentationAction<Destination.Action>)
+        case view(View)
+        case async(AsyncAction)
+        case inner(InnerAction)
+        case navigation(NavigationAction)
+        case signUpName(SignUpName.Action)
+        case signUpAge(SignUpAge.Action)
+        case signUpJob(SignUpJob.Action)
+        case activeTabChanged(SignUpTab)
+        
+    }
+    
+    @Reducer(state: .equatable)
+    public enum Destination {
+        case customPopUp(CustomPopUp)
+    }
+    
+    //MARK: - ViewAction
+    @CasePathable
+    public enum View {
+        case backSelectTab
+        case appearPopUp
+        case closePopUp
+        case presntOnboarding
+    }
+    
+  
+    
+    //MARK: - AsyncAction 비동기 처리 액션
+    public enum AsyncAction: Equatable {
+        case updateUserInfoResponse(Result<UpdateUserInfoModel, CustomError>)
+        case updateUserInfo(
+            nickName: String,
+            year: Int,
+            job: String,
+            generation: String)
+    }
+    
+    //MARK: - 앱내에서 사용하는 액션
+    public enum InnerAction: Equatable {
+       
+    }
+    
+    //MARK: - NavigationAction
+    public enum NavigationAction: Equatable {
+        case presntOnboarding
+    
+    }
+    
+    
+    @Dependency(SignUpUseCase.self) var signUpUseCase
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .binding(_):
+                return .none
+               
+            case .destination(_):
+                return .none
+                
+            case .activeTabChanged(let changeTab):
+                state.activeMenu = changeTab
+                return .none
+                
+            case .signUpName(.switchSettingTab):
+                state.activeMenu = .signUpGeneration
+//                state.selectedTab = 1
+                return .none
+                
+            case .signUpAge(.switchTabs):
+                state.activeMenu = .signUpJob
+//                state.selectedTab = 1
+                return .none
+                
+                
+//            case .signUpJob(.switchTabs):
+//                state.activeMenu = .signUpJob
+////                state.selectedTab = 1
+//                return .none
+                
+                
+                
+            case .view(let View):
+                switch View {
+                    
+               
+                case .backSelectTab:
+                    if state.activeMenu == .signUpGeneration {
+                        state.activeMenu = .signUpGeneration
+                    } else if state.activeMenu == .signUpGeneration {
+                        state.activeMenu = .signUpName
+                    }
+                    return .none
+                    
+                case .appearPopUp:
+                    state.destination = .customPopUp(.init())
+                    return .none
+                    
+                case .closePopUp:
+                    state.destination = nil
+                    return .none
+                    
+                    
+                case .presntOnboarding:
+                    return .none
+                }
+                
+            case .async(let AsyncAction):
+                switch AsyncAction {
+                    
+                case .updateUserInfo(let nickname, let year, let job, let generation):
+                    return .run { @MainActor send in
+                        let userInfoResult = await Result {
+                            try await signUpUseCase.updateUserInfo(nickname: nickname, year: year, job: job, generation: generation)
+                        }
+                        
+                        switch userInfoResult {
+                        case .success(let updateUserInfoData):
+                            if let updateUserInfoData = updateUserInfoData {
+                                send(.async(.updateUserInfoResponse(.success(updateUserInfoData))))
+                            }
+                            
+                            send(.view(.closePopUp))
+                            
+                            try await Task.sleep(nanoseconds: 500_000_000)
+                                send(.navigation(.presntOnboarding))
+                            
+                        case .failure(let error):
+                            send(.async(.updateUserInfoResponse(.failure(CustomError.map(error)))))
+                        }
+                    }
+                    
+                case .updateUserInfoResponse(let result):
+                    switch result {
+                    case .success(let userInfoData):
+                        state.updateUserinfoModel = userInfoData
+                    case let .failure(error):
+                        Log.error("update user info 리턴 에러", error)
+                    }
+                    return .none
+                }
+                
+            case .inner(let InnerAction):
+                switch InnerAction {
+               
+                }
+                
+            case .navigation(let NavigationAction):
+                switch NavigationAction {
+                case .presntOnboarding:
+                    return .none
+                }
+                
+                
+            default:
+                return .none
+            }
+        }
+        .ifLet(\.$destination, action: \.destination)
+        Scope(state: \.signUpName, action: \.signUpName) {
+            SignUpName()
+        }
+        Scope(state: \.signUpAge, action: \.signUpAge) {
+            SignUpAge()
+        }
+        Scope(state: \.signUpJob, action: \.signUpJob) {
+            SignUpJob()
+        }
+    }
+}
---
+++ OPeace/Projects/Presentation/Presentation/Sources/Profile/Main/View/ProfileView.swift
@@ -0,0 +1,144 @@
+//
+//  ProfileView.swift
+//  Presentation
+//
+//  Created by 서원지 on 7/31/24.
+//
+
+import SwiftUI
+import DesignSystem
+
+import ComposableArchitecture
+import PopupView
+import SwiftUIIntrospect
+
+import Utill
+
+public struct ProfileView: View {
+    @Bindable var store: StoreOf<Profile>
+
+    var backAction: () -> Void = {}
+    
+    
+    public init(
+        store: StoreOf<Profile>,
+        backAction: @escaping () -> Void
+    ) {
+        self.store = store
+        self.backAction = backAction
+    }
+    
+    public var body: some View {
+        ZStack {
+            Color.gray600
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                    .frame(height: 14)
+                
+                CustomTitleNaviagionBackButton(buttonAction: backAction, title: "마이페이지")
+                
+                userInfoTitle(
+                    nickName: store.profileUserModel?.data?.nickname ?? "",
+                    job: store.profileUserModel?.data?.job ?? "",
+                    generation:  store.profileGenerationText )
+                
+                Spacer()
+            }
+            .task {
+                store.send(.async(.fetchUser))
+            }
+            .introspect(.navigationStack, on: .iOS(.v17, .v18)) { navigationController in
+                navigationController.interactivePopGestureRecognizer?.isEnabled = true
+            }
+            .sheet(item: $store.scope(state: \.destination?.setting, action: \.destination.setting)) { settingStore in
+                SettingView(store: settingStore) {
+                    guard let settingtitem =  settingStore.settingtitem else {return}
+                    store.send(.view(.switchModalAction(settingtitem )))
+                } closeModalAction: {
+                    store.send(.view(.closeModal))
+                }
+                .presentationDetents([.height(UIScreen.screenHeight * 0.3)])
+                .presentationCornerRadius(20)
+                .presentationDragIndicator(.hidden)
+            }
+            
+            .popup(item: $store.scope(state: \.destination?.popup, action: \.destination.popup)) { customPopUp in
+                CustomBasicPopUpView(store: customPopUp, tittle: store.logoutPopUpTitle) {
+                    store.send(.async(.logoutUser))
+                } cancelAction: {
+                    store.send(.view(.closeModal))
+                }
+                
+            }  customize: { popup in
+                popup
+                    .type(.floater(verticalPadding: UIScreen.screenHeight * 0.35))
+                    .position(.bottom)
+                    .animation(.spring)
+                    .closeOnTap(true)
+                    .closeOnTapOutside(true)
+                    .backgroundColor(Color.basicBlack.opacity(0.8))
+            }
+        }
+    }
+}
+
+
+extension ProfileView {
+    
+    @ViewBuilder
+    private func userInfoTitle(nickName: String, job: String, generation: String) -> some View {
+        VStack {
+            Spacer()
+                .frame(height: 16)
+            
+            HStack {
+                Text(nickName)
+                    .pretendardFont(family: .SemiBold, size: 24)
+                    .foregroundStyle(Color.basicWhite)
+                
+                Spacer()
+                    .frame(width: 4)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.gray400, style: .init(lineWidth: 1.13))
+                    .frame(width:  job.calculateWidthProfile(for: job), height: 24)
+                    .background(Color.gray600)
+                    .overlay(alignment: .center) {
+                        Text(job)
+                            .pretendardFont(family: .Regular, size: 12)
+                            .foregroundStyle(Color.basicWhite)
+                    }
+                
+                Spacer()
+                    .frame(width: 4)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(store.profileGenerationColor)
+                    .frame(width: generation.calculateWidthProfileGeneration(for: generation), height: 24)
+                    .overlay(alignment: .center) {
+                        Text(generation)
+                            .pretendardFont(family: .Regular, size: 12)
+                            .foregroundStyle(store.profileGenerationTextColor)
+                    }
+                
+                
+                Spacer()
+                    .frame(width: 4)
+                
+                Image(asset: .setting)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .onTapGesture {
+                        store.send(.view(.tapPresntSettingModal))
+                    }
+                
+                Spacer()
+                
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
