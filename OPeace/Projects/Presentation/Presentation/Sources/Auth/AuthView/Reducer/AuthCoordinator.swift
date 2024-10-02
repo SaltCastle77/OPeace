3bed10a9bd74dd8e11b8bc216a0f9a91d2ecfc7c
Roy <shuwj81@daum.net>
2024-10-02 14:19:15 +0900

    ✨[feat]:  화면  전환  방식  변경   #13
     * navigationstack 에서 코디네이터 로 변경

---
+++ OPeace/Projects/Presentation/Presentation/Sources/Auth/AuthView/Reducer/AuthCoordinator.swift
@@ -0,0 +1,201 @@
+//
+//  Auth.swift
+//  Presentation
+//
+//  Created by 서원지 on 7/22/24.
+//
+
+import SwiftUI
+
+import AuthenticationServices
+
+import ComposableArchitecture
+import TCACoordinators
+
+import Utill
+import Networkings
+
+
+@Reducer
+public struct AuthCoordinator {
+    public init() {}
+    
+    @ObservableState
+    public struct State: Equatable {
+        var emptyModel: EmptyModel?
+        var login = Login.State()
+        var completeSignUp = SignUpPaging.State()
+        var onBoarding = OnBoadingPagging.State()
+        
+        var routes: [Route<AuthScreen.State>]
+        
+        
+        @Shared(.inMemory("isLogOut")) var isLogOut: Bool = false
+        @Shared(.inMemory("isDeleteUser")) var isDeleteUser: Bool = false
+        @Shared(.inMemory("isLookAround")) var isLookAround: Bool = false
+        @Shared(.inMemory("isChangeProfile")) var isChangeProfile: Bool = false
+        @Shared(.inMemory("createQuestionEmoji")) var emojiText: String = ""
+        @Shared(.inMemory("createQuestionTitle")) var createQuestionTitle: String = ""
+        @Shared(.inMemory("isCreateQuestion")) var isCreateQuestion: Bool = false
+        @Shared(.inMemory("isDeleteQuestion")) var isDeleteQuestion: Bool = false
+        @Shared(.inMemory("isReportQuestion")) var isReportQuestion: Bool = false
+        @Shared(.inMemory("questionID")) var questionID: Int = 0
+        @Shared(.inMemory("loginSocialType")) var loginSocialType: SocialType? = nil
+        
+        public init() {
+            self.routes = [.root(.login(.init()), embedInNavigationView: true)]
+        }
+        
+    }
+       
+    public enum Action: ViewAction ,FeatureAction {
+        case router(IndexedRouterActionOf<AuthScreen>)
+        case view(View)
+        case async(AsyncAction)
+        case inner(InnerAction)
+        case navigation(NavigationAction)
+        case login(Login.Action)
+        case completeSignUp(SignUpPaging.Action)
+        case onBoarding(OnBoadingPagging.Action)
+    }
+    
+  
+    
+    //MARK: - ViewAction
+    public enum View {
+        
+        
+    }
+    
+    //MARK: - AsyncAction 비동기 처리 액션
+    public enum AsyncAction {
+       
+        
+    }
+    
+    //MARK: - 앱내에서 사용하는 액션
+    public enum InnerAction: Equatable {
+       case removePath
+        case removeAllPath
+        case removeToHome
+    }
+    
+    //MARK: - NavigationAction
+    public enum NavigationAction: Equatable {
+        case presntMainHome
+    }
+    
+    @Dependency(AuthUseCase.self) var authUseCase
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .router(let routAction):
+                return routerAction(state: &state, action: routAction)
+                
+                
+            case .view(let View):
+                switch View {
+              
+                }
+                
+            case .async(let AsyncAction):
+                switch AsyncAction {
+                    
+                }
+                
+            case .inner(let InnerAction):
+                switch InnerAction {
+                case .removePath:
+                    state.routes.goBack()
+                    return .none
+                    
+                case .removeAllPath:
+                    return .none
+                    
+                case .removeToHome:
+                    return .none
+                }
+                
+            case .navigation(let NavigationAction):
+                switch NavigationAction {
+                case .presntMainHome:
+                    return .none
+                }
+            
+            default:
+                return .none
+            }
+        }
+        .forEachRoute(\.routes, action: \.router)
+        Scope(state: \.login, action: \.login) {
+            Login()
+        }
+        Scope(state: \.completeSignUp, action: \.completeSignUp) {
+            SignUpPaging()
+        }
+        Scope(state: \.onBoarding, action: \.onBoarding) {
+            OnBoadingPagging()
+        }
+        
+    }
+    
+    private func routerAction(
+            state: inout State,
+            action: IndexedRouterActionOf<AuthScreen>
+    ) -> Effect<Action> {
+        switch action {
+            //MARK: - Agree
+        case .routeAction(id: _, action: .login(.navigation(.presnetAgreement))):
+            state.routes.push(.agreeMent(.init()))
+            return .none
+
+            //MARK: - webView 동의
+        case .routeAction(id: _, action: .agreeMent(.navigation(.presntSignUpName))):
+            state.routes.push(.signUpPagging(.init()))
+            return .none
+            
+        case .routeAction(id: _, action: .agreeMent(.navigation(.presntServiceAgreeCheckTapped))):
+            state.routes.push(.webView(.init(url: AgreeMentAPI.seriveTerms.agreeMentDesc)))
+            return .none
+            
+        case .routeAction(id: _, action: .agreeMent(.navigation(.presntPrivacyAgreeCheckTapped))):
+            state.routes.push(.webView(.init(url: AgreeMentAPI.privacyPolicy.agreeMentDesc)))
+            return .none
+            
+            //MARK: - signUp
+        case .routeAction(id: _, action: .signUpPagging(.navigation(.presntOnboarding))):
+            state.routes.push(.onBoardingPagging(.init()))
+            return .none
+            
+        case .routeAction(id: _, action: .login(.navigation(.presntLookAround))):
+            return .send(.navigation(.presntMainHome))
+            
+        case .routeAction(id: _, action: .login(.navigation(.presentMain))):
+            return .send(.navigation(.presntMainHome))
+            
+        case .routeAction(id: _, action: .signUpPagging(.navigation(.presntMainHome))):
+            return .send(.navigation(.presntMainHome))
+            
+        case .routeAction(id: _, action: .onBoardingPagging(.navigation(.presntMainHome))):
+            return .send(.navigation(.presntMainHome))
+            
+        default:
+            return .none
+        }
+    }
+    
+}
+
+
+extension AuthCoordinator {
+    
+    @Reducer(state: .equatable)
+    public enum AuthScreen {
+        case login(Login)
+        case agreeMent(AgreeMent)
+        case signUpPagging(SignUpPaging)
+        case webView(Web)
+        case onBoardingPagging(OnBoadingPagging)
+    }
+}
